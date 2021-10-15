fda74706c57438237933729370ef36a48827a543
miam-miam100 <49870539+miam-miam100@users.noreply.github.com>
2021-10-16 00:35:47 +0100

    Finished challenge1

---
+++ challenge1/src/Cookie.java
@@ -0,0 +1,60 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class Cookie {
+  private static final String DIR = "data";
+  private static final String PATH = DIR + "\\cookie";
+  String name = "";
+  String value = "";
+
+  public Cookie() {
+    try {
+      File myFile = new File(PATH);
+      Scanner myReader = new Scanner(myFile);
+      name = myReader.nextLine();
+      value = myReader.nextLine();
+      myReader.close();
+    } catch (FileNotFoundException | NoSuchElementException e) {
+      Update();
+    }
+  }
+
+  /**
+   * Updates the cookie used by asking the user, it then stores the cookie in a file so that it can
+   * be used for later.
+   */
+  public void Update() {
+    // Get Values from User using Scanner
+    System.out.print(
+        "To update cookies please login into https://secure.ecs.soton.ac.uk/ and copy the cookie values here.\nName (Should start with _shibsession_): ");
+    Scanner in = new Scanner(System.in);
+    name = in.nextLine();
+    System.out.print("Cookie value: ");
+    value = in.nextLine();
+
+    try {
+      // Update values in file so that it can be used in future runs.
+      FileWriter writer = new FileWriter(PATH);
+      writer.write(name + "\n" + value);
+      writer.close();
+    } catch (IOException e) {
+      try {
+        // May have failed as dir does not exist.
+        Path path = Paths.get(DIR);
+        Files.createDirectory(path);
+        FileWriter writer = new FileWriter(PATH);
+        writer.write(name + "\n" + value);
+        writer.close();
+      } catch (IOException ioException) {
+        System.out.println("Could not create file to store cookies.");
+      }
+    }
+  }
+}
--- challenge1/src/Main.java
+++ challenge1/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -6,39 +7,56 @@ import org.jsoup.select.Elements;
 public class Main {
 
   public static void main(String[] args) {
-    // write your code here
-    String cookie_name = "";
-    String cookie_value = "";
-    Document doc;
-    try {
-      doc =
-          Jsoup.connect("https://secure.ecs.soton.ac.uk/people/?nameq=a")
-              .cookie(cookie_name, cookie_value)
-              .get();
-    } catch (java.io.IOException e) {
-      System.out.println("Could not connect to the web site... Are you connected to the internet?");
-      return;
-    }
-    if (doc.title().equals("Redirecting")) {
-      System.out.println(
-          "Could not access the secure website... Are you sure the cookie details are correct?");
-      return;
-    }
-    Elements table = doc.select("table[summary] tr ~ tr");
-    int count = 0;
-    for (Element col : table) {
-      count += 1;
-      String Name = col.select("td:eq(1) a[href^=\"https://\"]").text();
-      String Role = col.select("td:eq(2)").text();
-      String Email = col.select("td:eq(4) a").attr("href").substring(7);
-      String Phone = col.select("td:eq(5)").text();
-      if (!Phone.equals("")) {
-        Phone = ", Phone number: " + Phone;
+    Cookie cookie = new Cookie();
+    System.out.print("Please input your search query: ");
+    Scanner in = new Scanner(System.in);
+    // Removes all non letters and turns spaces into +
+    String query = in.nextLine().replaceAll("[^a-zA-Z\\s]", "").replaceAll("[\\s]", "+");
+
+    while (true) {
+      Document doc;
+      try {
+        System.out.println("Searching the database...");
+        // We use &picsize= so that the webpage doesn't send back photos and thus slow down our
+        // program.
+        doc =
+            Jsoup.connect("https://secure.ecs.soton.ac.uk/people/?nameq=" + query + "&picsize=")
+                .cookie(cookie.name, cookie.value)
+                .get();
+      } catch (java.io.IOException e) {
+        System.out.println(
+            "Could not connect to the web site... Are you connected to the internet?");
+        return;
       }
-      System.out.printf("Name: %s, Role: %s, Email: %s%s\n", Name, Role, Email, Phone);
-      if (count == 10) {
-        System.out.printf("Showed first %s results", count);
-        break;
+
+      if (doc.title().equals("Redirecting")) {
+        System.out.println(
+            "Could not access the secure website... Are you sure the cookie details are correct?");
+        cookie.Update();
+        continue;
+      }
+      // Get the table containing all the people in ecs that match the search query.
+      Elements table = doc.select("table[summary] tr ~ tr");
+      if (table.isEmpty()) {
+        System.out.println("Could not find the requested person...");
+        return;
+      }
+
+      int count = 0;
+      for (Element col : table) {
+        count += 1;
+        String Name = col.select("td:eq(0) a[href^=\"https://\"]").text();
+        String Role = col.select("td:eq(1)").text();
+        String Email = col.select("td:eq(3) a").attr("href").substring(7);
+        String Phone = col.select("td:eq(4)").text();
+        if (!Phone.isEmpty()) {
+          Phone = ", Phone number: " + Phone;
+        }
+        System.out.printf("Name: %s, Role: %s, Email: %s%s\n", Name, Role, Email, Phone);
+        if (count == 10) {
+          System.out.printf("Showed first %s results", count);
+          return;
+        }
       }
     }
   }
