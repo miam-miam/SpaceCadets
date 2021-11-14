e7d8926dec387030445244f3b15c84ba95d73f91
miam-miam100 <49870539+miam-miam100@users.noreply.github.com>
2021-11-14 04:51:58 +0000

    Added windows.

--- challenge5/src/Main.java
+++ challenge5/src/Main.java
@@ -1,7 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        Spirograph mySpirograph = new Spirograph(2,3,1);
-        mySpirograph.generate();
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BMPReader myWelcome = new BMPReader("res/welcome.bmp");
+    myWelcome.run();
+    int fixedRadius = readIntegerFromCmd(br, "fixed radius");
+    int movingRadius = readIntegerFromCmd(br, "moving radius");
+    int penOffset = readIntegerFromCmd(br, "pen offset");
+    BMPReader myGenerating = new BMPReader("res/generating.bmp");
+    myGenerating.run();
+    Spirograph mySpirograph = new Spirograph(fixedRadius, movingRadius, penOffset);
+    mySpirograph.run();
+  }
+
+  public static Integer readIntegerFromCmd(BufferedReader br, String name) {
+    while (true) {
+      try {
+        System.out.println("Enter your " + name + ":");
+        String input = br.readLine();
+        return Integer.valueOf(input);
+      } catch (NumberFormatException | IOException ignored) {
+      }
     }
+  }
 }
